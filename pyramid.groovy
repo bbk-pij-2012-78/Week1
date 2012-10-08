92068ddab0a99e31a4c7def9696edc3693053b8c
Nick Sutton <nsutton78@gmail.com>
2012-10-08 19:18:50 +0100

    Updated pyramid to come down from 10 to 1

--- pyramid.groovy
+++ pyramid.groovy
@@ -13,4 +13,20 @@ while (i < 10) {
         x++;
     }
 }
+
+i = 11;
+
+while (i > 1) {
+    i = i - 1;
+    x = 1;
+    while (x <= i) {
+        if (x == i) {
+            println(i) 
+        } else {
+            print(i)        
+        }
+        x++;
+    }    
+}
+
 println("finished");
